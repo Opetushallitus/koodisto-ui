5fa2832c0423aeb68874e6a33ba1a2ea4bd2dcc0
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-03-01 13:56:53 +0200

    add server build for container

---
+++ server/src/test/java/fi/vm/sade/koodisto/koodistoui/KoodistoUiApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.vm.sade.koodisto.koodistoui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoodistoUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
