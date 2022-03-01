4a4180c0c9b49dfc5c5f273e4e1a656ada53272d
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-03-01 15:57:45 +0200

    rename url path

--- src/InitializeApp/InitializeApp.tsx
+++ src/InitializeApp/InitializeApp.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 const virkailijaRaamitUrl = '/virkailija-raamit/apply-raamit.js';
 
 const InitializeApp: React.FC = ({ children }) => {
+    console.log(process.env);
     if (process.env.NODE_ENV === 'development' && !document.getElementById('virkailija-raamit-Script')) {
         const scriptElement = document.createElement('script');
-        scriptElement.src = '/koodisto-ui/dev-raamit.js';
+        scriptElement.src = '/koodisto-app/dev-raamit.js';
         scriptElement.id = 'virkailija-raamit-Script';
         document.body.appendChild(scriptElement);
     } else if (!document.getElementById('virkailija-raamit-Script')) {
