7f06cb4625bb6a6293efb4e0e40bdd83698f6168
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-03-01 12:20:06 +0200

    better linting

--- src/InitializeApp/InitializeApp.tsx
+++ src/InitializeApp/InitializeApp.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const virkailijaRaamitUrl = '/virkailija-raamit/apply-raamit.js';
 
-const InitializeApp: React.FC<{}> = ({ children }) => {
+const InitializeApp: React.FC = ({ children }) => {
     if (process.env.NODE_ENV === 'development' && !document.getElementById('virkailija-raamit-Script')) {
         const scriptElement = document.createElement('script');
         scriptElement.src = '/koodisto-ui/dev-raamit.js';
