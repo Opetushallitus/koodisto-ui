0e47814c88d5a0c4352694328c5e21485057f1d5
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-03-03 15:34:48 +0200

    mocking-environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useAtom } from 'jotai';
+import { codesAtom } from './api/codes';
 
 function App() {
+    const [data] = useAtom(codesAtom);
     return (
         <div>
             <h1>koodisto</h1>
+            {data.map((koodistoRyhma) => {
+                return (
+                    <div key={koodistoRyhma.id}>
+                        <h2>{koodistoRyhma.koodistoRyhmaUri}</h2>
+                        {koodistoRyhma.koodistos.map((koodisto) => {
+                            return <h3 key={koodisto.koodistoUri}>{koodisto.koodistoUri}</h3>;
+                        })}
+                    </div>
+                );
+            })}
         </div>
     );
 }
