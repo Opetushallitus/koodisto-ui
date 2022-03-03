0bf1b05300a13d6377cbe91e3bd580aa9c9ffc1c
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-03-03 08:52:42 +0200

    add headers

--- src/context/constants.ts
+++ src/context/constants.ts
@@ -1,2 +1,3 @@
 import { homepage } from '../../package.json';
 export const BASE_PATH = homepage;
+export const ROOT_OID = '1.2.246.562.10.00000000001';
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
 import App from './App';
 import InitializeApp from './InitializeApp/InitializeApp';
+import { ROOT_OID } from './context/constants';
+
+const cookies = new Cookies();
+axios.interceptors.request.use((config) => {
+    if (config?.headers) {
+        config.headers['Caller-Id'] = `${ROOT_OID}.koodisto-app`;
+        config.headers['CSRF'] = cookies.get('CSRF');
+    }
+    return config;
+});
 
 ReactDOM.render(
     <React.StrictMode>
