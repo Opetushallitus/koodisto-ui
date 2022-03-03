00e6ec311c97d7c6be219194a071ac95996db20e
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-03-03 16:50:43 +0200

    improve cypress

--- src/api/maxInterval.ts
+++
@@ -1,9 +0,0 @@
-import { API_BASE_PATH } from '../context/constants';
-import { atom } from 'jotai';
-import axios from 'axios';
-
-const urlAtom = atom(`${API_BASE_PATH}/session/maxinactiveinterval`);
-export const maxIntervalAtom = atom(async (get) => {
-    const response = await axios.get<number>(get(urlAtom));
-    return response.data;
-});
