4faf842b726c8a8b0b705eee27bee9d00aed5a56
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-03-16 23:57:17 +0200

    logging

--- src/api/kayttooikeus.ts
+++ src/api/kayttooikeus.ts
@@ -14,8 +14,11 @@ type CASMeApi = {
 const urlAtom = atom<string>('/kayttooikeus-service/cas/me');
 export const casMeAtom = atom<Promise<CASMeApi>>(async (get: Getter) => {
     const { data } = await axios.get<CASMeApi>(get(urlAtom));
+    console.log('casMeAtom data:', data);
     return data;
 });
 export const casMeLangAtom = atom((get) => {
-    return get(casMeAtom).lang;
+    const casMe = get(casMeAtom);
+    console.log('casMe data:', casMe);
+    return casMe.lang;
 });
