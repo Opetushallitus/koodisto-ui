94c364b144a9dd7c148d2160a464c566bbb382c1
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-03-16 14:17:15 +0200

    remove kopypasta

--- server/src/main/java/fi/vm/sade/koodisto/config/DevUserDetailsServiceConfiguration.java
+++ server/src/main/java/fi/vm/sade/koodisto/config/DevUserDetailsServiceConfiguration.java
@@ -16,20 +16,6 @@ import java.util.List;
 @Profile("dev")
 @Configuration
 public class DevUserDetailsServiceConfiguration {
-    private static final SimpleGrantedAuthority[] OPH_AUTHORITIES = new SimpleGrantedAuthority[]{
-            new SimpleGrantedAuthority("ROLE_APP_ORGANISAATIOHALLINTA_CRUD_1.2.246.562.10.00000000001"),
-            new SimpleGrantedAuthority("ROLE_APP_ORGANISAATIOHALLINTA"),
-            new SimpleGrantedAuthority("ROLE_APP_ORGANISAATIOHALLINTA_CRUD")
-    };
-    private static final SimpleGrantedAuthority[] RESTRICTED_AUTHORITIES = new SimpleGrantedAuthority[]{
-            new SimpleGrantedAuthority("ROLE_APP_ORGANISAATIOHALLINTA")
-    };
-    private static final SimpleGrantedAuthority[] ESPOO_AUTHORITIES = new SimpleGrantedAuthority[]{
-            new SimpleGrantedAuthority("APP_ORGANISAATIOHALLINTA"),
-            new SimpleGrantedAuthority("APP_ORGANISAATIOHALLINTA_CRUD"),
-            new SimpleGrantedAuthority("APP_ORGANISAATIOHALLINTA_CRUD_1.2.246.562.10.90008375488"),
-    };
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -51,26 +37,10 @@ public class DevUserDetailsServiceConfiguration {
 
         @Override
         public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-            switch (username) {
-                case "restricted":
-                    return User.builder()
-                            .authorities(List.of(RESTRICTED_AUTHORITIES))
-                            .password(this.passwordEncoder.encode(username))
-                            .username(username)
-                            .build();
-                case "espoo":
-                    return User.builder()
-                            .authorities(List.of(ESPOO_AUTHORITIES))
-                            .password(this.passwordEncoder.encode(username))
-                            .username(username)
-                            .build();
-                default:
-                    return User.builder()
-                            .authorities(List.of(OPH_AUTHORITIES))
-                            .password(this.passwordEncoder.encode("devaaja"))
-                            .username("devaaja")
-                            .build();
-            }
+            return User.builder()
+                    .password(this.passwordEncoder.encode("devaaja"))
+                    .username("devaaja")
+                    .build();
         }
     }
 }
