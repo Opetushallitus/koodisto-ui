074470da764887034b92f3abbf406a1796292b0d
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-03-22 17:51:59 +0200

    add test

--- src/utils/downloadCsv.ts
+++ src/utils/downloadCsv.ts
@@ -40,11 +40,10 @@ const pushBlobToUser = ({ fileName, blob }: { fileName: string; blob: Blob }) =>
 
 const convertCsvToExcelAcceptedBlob = (csv: string) => {
     const csvWithBom = `${decodeURIComponent('%EF%BB%BF')}${csv}`;
-    const csvArray = new Uint16Array(
-        csvWithBom.split('').map(function (k) {
-            return k.charCodeAt(0);
-        })
-    );
+    const charCodeArray = Array.from(csvWithBom).map(function (k) {
+        return k.charCodeAt(0);
+    });
+    const csvArray = new Uint16Array(charCodeArray);
     return new Blob([csvArray], { type: 'text/csv;charset=UTF-16LE;' });
 };
 
