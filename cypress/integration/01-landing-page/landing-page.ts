70c8153ecf2edc9ad713d09c8e7f84c8c28e06d1
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-05-25 10:22:26 +0300

    fix cypress

--- cypress/integration/01-landing-page/landing-page.ts
+++ cypress/integration/01-landing-page/landing-page.ts
@@ -2,7 +2,6 @@ import { API_INTERNAL_PATH, BASE_PATH } from '../../../src/context/constants';
 
 describe('The landing page', () => {
     it('shows koodistos on landing page', () => {
-        cy.intercept('kayttooikeus-service/cas/me', { fixture: 'cas.me.json' });
         cy.intercept(`${API_INTERNAL_PATH}/koodisto`, { fixture: 'codes.json' });
         cy.visit(`${BASE_PATH}`);
         cy.contains('aluehallintovirasto').should('be.visible');
