8755958329c79f5b37a782442f7ce17be47ebc14
Lauri Arimo <lauri.arimo@wunderdog.fi>
2022-06-09 14:07:49 +0300

    KH-355: Add cypress test for koodi table filter

--- cypress/integration/02-koodisto-page/koodisto-page.ts
+++ cypress/integration/02-koodisto-page/koodisto-page.ts
@@ -9,4 +9,16 @@ describe('The Koodisto View page', () => {
         cy.visit(`${BASE_PATH}/koodisto/kunta/2`);
         cy.contains('kunta').should('be.visible');
     });
+    it('Koodi list may be filtered', () => {
+        cy.intercept(`${API_INTERNAL_PATH}/koodisto/kunta/2`, { fixture: 'kuntaKoodisto.json' });
+        cy.visit(`${BASE_PATH}/koodisto/kunta/2`);
+        cy.get('.accordion > div').last().click();
+        cy.get('tbody > tr:visible').should('have.length', 630);
+        cy.get('input').last().type('mouhijärvi');
+        // filter by name
+        cy.get('tbody > tr:visible').should('have.length', 1);
+        cy.focused().clear().type('493');
+        // filter by koodiArvo
+        cy.get('tbody > tr:visible').should('have.length', 1);
+    });
 });
