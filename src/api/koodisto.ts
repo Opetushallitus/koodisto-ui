5923e43e178c774bae99967a478623f96c67dd12
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-08-12 10:44:19 +0300

    cleanup

--- src/api/koodisto.ts
+++ src/api/koodisto.ts
@@ -221,20 +221,19 @@ const toApiRelation = (relation: KoodistoRelation): ApiKoodistoRelation => ({
     codesVersion: relation.koodistoVersio,
     passive: !!relation.passive,
 });
-function mapPageKoodistoToUpdatePageKoodisto(koodisto: PageKoodisto): UpdateKoodistoDataType {
-    return {
-        includesCodes: koodisto.sisaltaaKoodistot.map(toApiRelation),
-        levelsWithCodes: koodisto.rinnastuuKoodistoihin.map(toApiRelation),
-        withinCodes: koodisto.sisaltyyKoodistoihin.map(toApiRelation),
-        version: koodisto.lockingVersion,
-        tila: koodisto.tila,
-        versio: koodisto.versio,
-        codesGroupUri: koodisto.koodistoRyhmaUri.value,
-        koodistoUri: koodisto.koodistoUri,
-        metadata: koodisto.metadata,
-        ...mapPageKoodistoToCreatePageKoodisto(koodisto),
-    };
-}
+
+const mapPageKoodistoToUpdatePageKoodisto = (koodisto: PageKoodisto): UpdateKoodistoDataType => ({
+    includesCodes: koodisto.sisaltaaKoodistot.map(toApiRelation),
+    levelsWithCodes: koodisto.rinnastuuKoodistoihin.map(toApiRelation),
+    withinCodes: koodisto.sisaltyyKoodistoihin.map(toApiRelation),
+    version: koodisto.lockingVersion,
+    tila: koodisto.tila,
+    versio: koodisto.versio,
+    codesGroupUri: koodisto.koodistoRyhmaUri.value,
+    koodistoUri: koodisto.koodistoUri,
+    metadata: koodisto.metadata,
+    ...mapPageKoodistoToCreatePageKoodisto(koodisto),
+});
 
 export const fetchPageKoodisto = async ({
     koodistoUri,
@@ -244,8 +243,8 @@ export const fetchPageKoodisto = async ({
     koodistoUri: string;
     versio?: number;
     lang: Kieli;
-}): Promise<PageKoodisto | undefined> => {
-    return errorHandlingWrapper(async () => {
+}): Promise<PageKoodisto | undefined> =>
+    errorHandlingWrapper(async () => {
         const { data: apiPageKoodisto } = await axios.get<ApiPageKoodisto>(
             [API_INTERNAL_PATH, 'koodisto', koodistoUri, ...(versio ? [versio] : [])].join('/')
         );
@@ -256,13 +255,11 @@ export const fetchPageKoodisto = async ({
             return undefined;
         }
     });
-};
 
-export const deleteKoodisto = async (koodisto: PageKoodisto): Promise<boolean | undefined> => {
-    return errorHandlingWrapper(async () => {
+export const deleteKoodisto = async (koodisto: PageKoodisto): Promise<boolean | undefined> =>
+    errorHandlingWrapper(async () => {
         const { status } = await axios.delete(
             `${API_INTERNAL_PATH}/koodisto/${koodisto.koodistoUri}/${koodisto.versio}`
         );
         return status === 204;
     });
-};
--- src/components/utils/ConditionalWrapper.tsx
+++
@@ -1,6 +0,0 @@
-import React, { ReactNode } from 'react';
-
-export const ConditionalWrapper: React.FC<{
-    condition: boolean;
-    wrapper: (children?: ReactNode) => ReactNode;
-}> = ({ condition, wrapper, children }) => (condition ? <>{wrapper(children)}</> : <>{children}</>);
