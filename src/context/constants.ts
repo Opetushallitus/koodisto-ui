67914db97eed242975f8785c75c08f1fdded8c8b
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-09-22 14:07:40 +0300

    KH-378 fixed organisations in selector (#64)

--- src/context/constants.ts
+++ src/context/constants.ts
@@ -5,3 +5,8 @@ export const API_LOKALISAATIO_PATH = '/lokalisointi/cxf/rest/v1/localisation';
 export const API_BASE_PATH = '/koodisto-service/rest';
 export const API_INTERNAL_PATH = '/koodisto-service/internal';
 export const API_STATUS_PATH = '/koodisto-service/rest/session/maxinactiveinterval';
+export const ORGANISAATIO_URI_LIST = [
+    '1.2.246.562.10.00000000001',
+    '1.2.246.562.10.2013112012294919827487',
+    '1.2.246.562.10.78946234170',
+];
